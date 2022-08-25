b7d90cab7e50f6718814651262841d81572cfa23
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-25 19:12:36 +0530

    Create DataFrame

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from os import getcwd
+import pandas as pd
+##############################################
 
 app = FastAPI()
+
+# Create `df` DataFrame 
+df = pd.read_csv(f'{getcwd()}/titanic.csv')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -13,3 +18,4 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
